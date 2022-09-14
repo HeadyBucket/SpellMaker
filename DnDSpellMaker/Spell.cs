2f787702c12bdad0e7d53f3c1b3e325edb8db4a3
HeadyBucket <14062540+HeadyBucket@users.noreply.github.com>
2022-09-14 19:13:54 +0100

    Add project files.

---
+++ DnDSpellMaker/Spell.cs
@@ -0,0 +1,39 @@
+﻿namespace DnDSpellMaker
+{
+    public class Spell
+    {
+        string name;
+        string flavour;
+
+        int totalEnergyCost;
+        
+        HashSet<Effect> effects;
+        Tradition tradition;
+    }
+
+    //effects are the mechanical and practical results of the spell, the damage it deals and any impact it has on the world
+    public class Effect
+    {
+        string name;
+        int energyCost;
+
+        //normalEffect;
+
+        //determines how an effect applies such as what targets can be hit or how long an Effect lasts
+        //requirement;
+
+    }
+
+    public class Trigger
+    {
+        string name;
+
+    }
+
+
+
+    public class Tradition
+    {
+        string name;
+    }
+}
